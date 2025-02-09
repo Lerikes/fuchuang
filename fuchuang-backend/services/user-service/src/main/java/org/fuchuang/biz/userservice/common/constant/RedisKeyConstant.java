2c85735e028c3369ab9119e4262f7ae51d93bf6f
yuanrui.513 <yuanrui.513@bytedance.com>
2025-02-09 13:57:41 +0800

    删除无用的常量

--- fuchuang-backend/services/user-service/src/main/java/org/fuchuang/biz/userservice/common/constant/RedisKeyConstant.java
+++ fuchuang-backend/services/user-service/src/main/java/org/fuchuang/biz/userservice/common/constant/RedisKeyConstant.java
@@ -6,11 +6,6 @@ package org.fuchuang.biz.userservice.common.constant;
 public final class RedisKeyConstant {
 
     /**
-     * 用户注册可复用用户名分片，Key Prefix + Idx
-     */
-    public static final String USER_REGISTER_REUSE_SHARDING = "fuchuang-user-service:user-reuse:";
-
-    /**
      * 用户登录验证码，Key Prefix + email
      */
     public static final String USER_LOGIN_VERIFY_CODE = "fuchuang-user-service:user-verifyCode:";
